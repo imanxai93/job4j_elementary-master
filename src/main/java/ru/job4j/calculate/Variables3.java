02e26e3fbb88f3302a29190d138dfc2729dec8a7
progprog-1907 <progprog1907@gmail.com>
2023-02-05 23:54:37 +0400

    2. Переменные. [#172635 #163776]

---
+++ src/main/java/ru/job4j/calculate/Variables3.java
@@ -0,0 +1,29 @@
+package ru.job4j.calculate;
+
+public class Variables3 {
+    public static void main(String[] args) {
+        byte age = 27; // для возраста
+        System.out.println(age);
+
+        char gender = 'M'; // для пол-а
+        System.out.println(gender);
+
+        float height = 180.7f; // для роста
+        System.out.println(height);
+
+        short bricks = 10000; // для количество кирпиджей
+        System.out.println(bricks);
+
+        long bacterium = 12784216711049L; // для бактерий
+        System.out.println(bacterium);
+
+        int objects = 45000; // для объектов
+        System.out.println(objects);
+
+        boolean workPermit = false; // для разрешения на работу
+        System.out.println(workPermit);
+
+        double fileSize = 164797918.5; // для размера файла
+        System.out.println(fileSize);
+    }
+}
