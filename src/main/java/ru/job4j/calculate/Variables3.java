177df5960d75d97f5e35993c446b00075e1da94a
progprog-1907 <progprog1907@gmail.com>
2023-02-10 02:07:42 +0400

    9.3. Математические операторы и составные операторы присваивания [#504905]

--- src/main/java/ru/job4j/calculate/Variables3.java
+++ src/main/java/ru/job4j/calculate/Variables3.java
@@ -2,28 +2,28 @@ package ru.job4j.calculate;
 
 public class Variables3 {
     public static void main(String[] args) {
-        byte age = 27; // для возраста
+        byte age = 27;
         System.out.println(age);
 
-        char gender = 'M'; // для пол-а
+        char gender = 'M';
         System.out.println(gender);
 
-        float height = 180.7f; // для роста
+        float height = 180.7f;
         System.out.println(height);
 
-        short bricks = 10000; // для количество кирпиджей
+        short bricks = 10000;
         System.out.println(bricks);
 
-        long bacterium = 12784216711049L; // для бактерий
+        long bacterium = 12784216711049L;
         System.out.println(bacterium);
 
-        int objects = 45000; // для объектов
+        int objects = 45000;
         System.out.println(objects);
 
-        boolean workPermit = false; // для разрешения на работу
+        boolean workPermit = false;
         System.out.println(workPermit);
 
-        double fileSize = 164797918.5; // для размера файла
+        double fileSize = 164797918.5;
         System.out.println(fileSize);
     }
 }
