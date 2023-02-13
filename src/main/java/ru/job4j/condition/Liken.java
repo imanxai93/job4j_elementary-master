558aa141d447afee1b6f3e4fcfb3a2c29c78d1c8
progprog-1907 <progprog1907@gmail.com>
2023-02-13 16:35:25 +0400

    0. Операции сравнения [#235631 #165842]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -2,16 +2,14 @@ package ru.job4j.condition;
 
 public class Liken {
     public static void main(String[] args) {
-        int first = 10;
-        int second = 9;
 
-        boolean result = first > second;
+        boolean result = true;
         System.out.println(result);
 
-        result = first < second;
+        result = false;
         System.out.println(result);
 
-        result = first == second;
+        result = true;
         System.out.println(result);
     }
 }
