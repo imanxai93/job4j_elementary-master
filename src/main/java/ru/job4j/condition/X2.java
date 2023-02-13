ceb249e73574d54b17f7450aefd7982a72624cb9
progprog-1907 <progprog1907@gmail.com>
2023-02-13 14:18:02 +0400

    1.2. Входные данные в тесте [#297750 #165511]

---
+++ src/main/java/ru/job4j/condition/X2.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+public class X2 {
+    public static int calc(int a, int b, int c, int x) {
+        return a * x * x + b * x + c;
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 2;
+        int rsl = calc(a, b, c, x);
+        System.out.println(rsl);
+        a = 1;
+        b = 1;
+        c = 1;
+        x = 0;
+        rsl = calc(a, b, c, x);
+        System.out.println(rsl);
+    }
+}
